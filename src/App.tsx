ad63b94a1825d8e2639df244a7e125bd6252b7d5
cgnz0r <60849962+cgnz0r@users.noreply.github.com>
2021-02-10 22:10:01 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import { React } from "react";
+import { observer } from "mobx-react";
+
+function App() {
+  return (<><h1>Hello motherfucka</h1></>);
+}
+
+export default App;
